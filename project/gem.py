a219e97f7d7052eff45936ae06faddda6fa873a3
DESKTOP-F6DG3P0\HP <dragon15098@gmail.com>
2017-03-11 21:55:07 +0700

    thêm gem

---
+++ project/gem.py
@@ -0,0 +1,4 @@
+class Gem:
+    def __init__(self, gx, gy):
+        self.gem_x = gx
+        self.gem_y = gy
--- project/map.py
+++ project/map.py
@@ -1,15 +1,18 @@
 import pygame
 from player import Player
 import random
+from gem import Gem
 
 class Map:
     def __init__(self, width, height):
         self.width = width
         self.height = height
         self.player = Player(random.randint(0,self.width - 1), random.randint(0,self.height - 1))
+        self.gem = Gem(random.randint(0,self.width - 1), random.randint(0,self.height - 1))
 
     def print_map(self):
         pygame.init()
+        print(self.gem.gem_x, "    " , self.gem.gem_y)
         screen = pygame.display.set_mode([800, 600])
         done = False
         bg_image = pygame.image.load("bg_image.png")
@@ -28,11 +31,14 @@ class Map:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     done = True
+
             screen.fill(COLOR_WHITE)
             for y in range(self.height):
                 for x in range(self.width):
                     screen.blit(bg_image, (x * SQUARE_SIZE, y * SQUARE_SIZE))
             screen.blit(p_image, (self.player.pox* SQUARE_SIZE, self.player.poy * SQUARE_SIZE))
+        pygame.display.flip()
+
 
 
 
