20366f52fb7911db66055c179e306bb20c6de7ca
DESKTOP-F6DG3P0\HP <dragon15098@gmail.com>
2017-03-17 20:59:15 +0700

    jhgfcgvhsbjnkdc

--- project/models/test.py
+++ project/models/test.py
@@ -6,8 +6,3 @@ for i in range(100):
 for j in range(100):
     map = Map()
     assert map.player.dic_player["x"] != map.bat.dic_bat["x"] or map.player.dic_player["y"] != map.bat.dic_bat["y"]
-
-for x in range(100):
-    map = Map()
-    for i in range(len(map.hole.list_hole)):
-        assert map.hole.list_hole[i]["x"] != map.gem.dic_gem["x"] or map.hole.list_hole[i]["y"] != map.gem.dic_gem["y"]
