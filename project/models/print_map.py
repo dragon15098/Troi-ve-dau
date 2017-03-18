6a3c9165c6e4ca194adfbbdaf50a3a0c56fdea87
DESKTOP-F6DG3P0\HP <dragon15098@gmail.com>
2017-03-18 20:44:20 +0700

    tạm

--- project/models/print_map.py
+++ project/models/print_map.py
@@ -8,11 +8,11 @@ pygame.init()
 screen = pygame.display.set_mode([800, 600])
 done = False
 map = Map()
-bg_image = pygame.image.load("images/thuy.jpg")
-p_image = pygame.image.load("images/mario.png")
-gem_image = pygame.image.load("images/box.png")
-hole_image= pygame.image.load("images/1.jpg")
-bat_image = pygame.image.load("images/doi.png")
+bg_image = pygame.image.load("images/water.jpg")
+# p_image = pygame.image.load("images/mario.png")
+# gem_image = pygame.image.load("images/box.png")
+# hole_image= pygame.image.load("images/1.jpg")
+# bat_image = pygame.image.load("images/doi.png")
 
         # draw bg
 COLOR_WHITE = (255, 255, 255)
@@ -26,20 +26,20 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                    dx = -1
-            elif event.key == pygame.K_RIGHT:
-                    dx = 1
-            elif event.key == pygame.K_UP:
-                    dy = -1
-            elif event.key == pygame.K_DOWN:
-                        dy = 1
-            else:
-                    dx, dy = 0, 0
-
-        if dx != 0 or dy != 0:
-            map.move_player(dx, dy)
+        # elif event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_LEFT:
+        #             dx = -1
+        #     elif event.key == pygame.K_RIGHT:
+        #             dx = 1
+        #     elif event.key == pygame.K_UP:
+        #             dy = -1
+        #     elif event.key == pygame.K_DOWN:
+        #                 dy = 1
+        #     else:
+        #             dx, dy = 0, 0
+        #
+        # if dx != 0 or dy != 0:
+        #     map.move_player(dx, dy)
 
 
     screen.fill(COLOR_WHITE)
@@ -47,8 +47,8 @@ while not done:
             #     for x in range(self.width):
             #         screen.blit(bg_image, (x * SQUARE_SIZE, y * SQUARE_SIZE))
     screen.blit(bg_image, (0 ,0))
-    screen.blit(p_image, (map.player.dic_player["x"] * SQUARE_SIZE, map.player.dic_player["y"] * SQUARE_SIZE))
-    screen.blit(gem_image, (map.gem.dic_gem["x"] * SQUARE_SIZE, map.gem.dic_gem["y"] * SQUARE_SIZE))
-    screen.blit(hole_image, (map.hole.list_hole[0]["x"] * SQUARE_SIZE, map.hole.list_hole[0]["y"] * SQUARE_SIZE))
-    screen.blit(bat_image, (map.bat.dic_bat[0]["x"] * SQUARE_SIZE, map.bat.dic_bat[0]["y"] * SQUARE_SIZE))
+    # screen.blit(p_image, (map.player.dic_player["x"] * SQUARE_SIZE, map.player.dic_player["y"] * SQUARE_SIZE))
+    # screen.blit(gem_image, (map.gem.dic_gem["x"] * SQUARE_SIZE, map.gem.dic_gem["y"] * SQUARE_SIZE))
+    # screen.blit(hole_image, (map.hole.list_hole[0]["x"] * SQUARE_SIZE, map.hole.list_hole[0]["y"] * SQUARE_SIZE))
+    # screen.blit(bat_image, (map.bat.dic_bat[0]["x"] * SQUARE_SIZE, map.bat.dic_bat[0]["y"] * SQUARE_SIZE))
     pygame.display.flip()
