e262541821aace41174aef37ea2a517cbbf0ce58
DESKTOP-F6DG3P0\HP <dragon15098@gmail.com>
2017-03-19 13:30:25 +0700

    11ting

--- project/views/print_map.py
+++ project/views/print_map.py
@@ -15,9 +15,9 @@ def print_map(map, screen):
     screen.blit(bg_image, (0, 0))
     screen.blit(p_image, (map.player.dic_player["x"] * SQUARE_SIZE, map.player.dic_player["y"] * SQUARE_SIZE))
     screen.blit(gem_image, (map.gem.dic_gem["x"] * SQUARE_SIZE, map.gem.dic_gem["y"] * SQUARE_SIZE))
-    screen.blit(hole_image, (map.hole.list_hole[0]["x"] * SQUARE_SIZE, map.hole.list_hole[0]["y"] * SQUARE_SIZE))
+    for i in range (len(map.hole.list_hole)):
+        screen.blit(hole_image, (map.hole.list_hole[i]["x"] * SQUARE_SIZE, map.hole.list_hole[i]["y"] * SQUARE_SIZE))
     screen.blit(bat_image, (map.bat.dic_bat["x"] * SQUARE_SIZE, map.bat.dic_bat["y"] * SQUARE_SIZE))
-
 
 def print_text_box(screen):
     text_box = pygame.image.load("../images/text_box.png")
